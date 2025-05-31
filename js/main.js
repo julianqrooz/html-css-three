b547aff10f687d37f3a21c7617ce00eae3e2ba89
rabea <julianqrooz@gmail.com>
2025-05-31 22:58:21 +0300

    Animate Width On Scrolling and Increase Numbers On Scrolling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
+let nums = document.querySelectorAll(".stats .number");
+let statssection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+let section = document.querySelector(".our-skills");
+let spans = document.querySelectorAll(".the-progress span");
 let countdowndate = new Date("Dec 31, 2025 23:59:59").getTime();
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
 
 let counter = setInterval(() => {
   let datenow = new Date().getTime();
-
   let datediff = countdowndate - datenow;
-
   let days = Math.floor(datediff / (1000 * 60 * 60 * 24));
   let hours = Math.floor((datediff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((datediff % (1000 * 60 * 60)) / (1000 * 60));
@@ -15,8 +28,21 @@ let counter = setInterval(() => {
     minutes < 10 ? `0${minutes}` : minutes;
   document.querySelector(".seconds").innerHTML =
     seconds < 10 ? `0${seconds}` : seconds;
-
   if (datediff < 0) {
     clearInterval(counter);
   }
 }, 1000);
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 600) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  if (window.scrollY >= statssection.offsetTop - 600) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
